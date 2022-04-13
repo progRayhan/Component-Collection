09fa37d269b3636ef837bad8d4ba981823f1c189
Md Rezaul Karim <73929234+bdrkreza@users.noreply.github.com>
2022-04-13 13:01:13 +0600

    update

--- pages/component_adder.jsx
+++ pages/component_adder.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import CarDetails from "../components/carDetails";
 import CarFeatures from "../components/CarFeatures";
+import CarAskingPrice from "../components/carAskingPice";
 
 function component_adder() {
   return (
     <div>
       <CarDetails />
       <CarFeatures />
+    <CarAskingPrice/>
     </div>
   );
 }
